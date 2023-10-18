aac296547d11dc48a56e5ea45b82d1f690a1700e
thisism-ali <124588015+mohalibs@users.noreply.github.com>
2023-10-18 09:24:41 +0100

    'commit'

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AM.Infrastructure
+{
+    public class AMContext : DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+            Initial Catalog=AirportManagementDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
